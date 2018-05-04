5442771709753be831d4322444d6d127a2295e32
maxiao1993 <soundwave_max@163.com>
2018-05-04 21:22:37 +0800

    fix the Graph problem

--- Graph/graph.h
+++ Graph/graph.h
@@ -1,6 +1,33 @@
 
+
 #define UNVISITED 0
 #define VISITED 1
+
+
+class Graph{
+	public:
+	//Return the number of vertices
+	virtual int n() =0;
+	//Return the current number of edges
+	virtual int e() =0;
+	//Return the index of the first neigbor of given vertex
+	virtual int first(int) =0;
+	//Return the index of the next neigbor of given vertex
+	virtual int next(int, int) =0;
+	//Store an edge defined by two vertices and weight
+	virtual void setEdge(int, int, int) =0;
+	//Delete edge defined by two vertices
+	virtual void delEdge(int, int) =0;
+	//Return weight of edge connecting two vertices
+	//Return 0 if no such edge exits
+	virtual int weight(int, int) =0;
+	//Get mark value for a vertex
+	virtual int getMark(int) =0;
+	//Set mark value for a vertex
+	virtual void setMark(int, int) =0;
+};
+
+
 
 class Edge {
 public:
@@ -9,7 +36,7 @@ public:
 	Edge(int v, int w) {vertex = v; weight = w;}
 };
 
-class Graphm {
+class Graphm : public Graph{
 private:
 	int numVertex, numEdge;		//Store number of vertices, edges
 	int **matrix;			//Pointer to adjacency matrix
